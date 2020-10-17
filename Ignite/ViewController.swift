1e66ae99ea25dfd53ecbee0293a2c77642022ae1
ageor0114 <austinninja362@gmail.com>
2020-10-17 15:03:15 -0400

    Initial Commit

---
+++ Ignite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ignite
+//
+//  Created by ageor0114 on 10/17/20.
+//  Copyright © 2020 Ignite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
