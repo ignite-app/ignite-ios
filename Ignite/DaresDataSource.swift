ebd6490b87db972d00fac33ead1511da8173b72c
Kathlee Wong <ksuwong@berkeley.edu>
2020-11-17 22:22:12 -0800

    just code for dare prompt and added new class for dares data source

---
+++ Ignite/DaresDataSource.swift
@@ -0,0 +1,18 @@
+//
+//  DaresDataSource.swift
+//  Ignite
+//
+//  Created by Kathlee  Wong  on 11/17/20.
+//  Copyright © 2020 Ignite. All rights reserved.
+//
+
+import Foundation
+
+class DaresDataSource {
+    let dares = ["Eat a spoonful of siracha",
+                 "Tweet any message at your friend’s request",
+                 "Text your crush and say it was accident",
+                 "Record yourself doing the WAP dance",
+                 "Do 15 push-ups",
+                 "Chug a glass of milk"]
+}
